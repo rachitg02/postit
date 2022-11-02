7f2aff9e1ed2a7baf98a99c06a194c1462271862
Rachit Gupta <rachitgup02@gmail.com>
2022-11-02 18:32:23 +0530

    page content layout

--- src/pages/p/[communityId]/index.tsx
+++ src/pages/p/[communityId]/index.tsx
@@ -6,6 +6,7 @@ import { firestore } from '../../../firebase/clientApp';
 import safeJsonStringify from 'safe-json-stringify'
 import NotFound from '../../../components/Community/NotFound';
 import Header from '../../../components/Community/Header';
+import PageContent from '../../../components/Layout/PageContent';
 
 type CommunityPageProps = {
     communityData: Community;
@@ -19,6 +20,15 @@ const CommunityPage:React.FC<CommunityPageProps> = ({communityData}) => {
     return (
         <>
         <Header communityData={communityData}/>
+        <PageContent>
+            <>
+            <div>LHS</div>
+            </>
+            
+            <>
+            <div>RHS</div>
+            </>
+        </PageContent>
         </>
     )
 }
